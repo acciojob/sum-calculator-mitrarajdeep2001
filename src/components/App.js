2c185b6785d2a7254927a1e273e5e25b9df97056
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-22 11:02:23 +0000

    fourth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
-
-import React, {useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-const [sum, setSum] = useState(0)
-function calculateSum(e){
-  
-  setSum(sum + +e.target.value)
-}
-  return (
-    <div>
-        <h1>Sum Calculator</h1>
-        <input type="number" onClick={calculateSum}/>
-        <p>Sum: {sum}</p>
-    </div>
-  )
+const [numbers, setNumbers] = useState([]);
+const [sum, setSum] = useState(0);
+
+function calculateSum(e) {
+const inputValue = +e.target.value;
+
+setNumbers((prevNumbers) => [...prevNumbers, inputValue]);
 }
 
-export default App
+useEffect(() => {
+const calculateSumAsync = async () => {
+const newSum = numbers.reduce((acc, curr) => acc + curr, 0);
+setSum(newSum);
+};
+
+calculateSumAsync();
+}, [numbers]);
+
+return (
+<div>
+<h1>Sum Calculator</h1>
+<input type="number" onChange={calculateSum} />
+<p>Sum: {sum}</p>
+</div>
+);
+};
+
+export default App;
